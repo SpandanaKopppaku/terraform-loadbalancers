46e4eca9afa59e789d92a1b1e5741c79d12196b3
spandanakoppaku <skoppaku@gmail.com>
2023-08-10 10:58:42 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -2,12 +2,14 @@ module "alb-public" {
 source                      = "./vendor/modules/alb" 
 ENV                         = var.ENV
 ALB_NAME                    = "roboshop-public-alb"
+INTERNAL                    = false
 }
 
 module "alb-private" {
 source                      = "./vendor/modules/alb" 
 ENV                         = var.ENV
 ALB_NAME                    = "roboshop-private-alb"
+INTERNAL                    = true
 }
 
 
